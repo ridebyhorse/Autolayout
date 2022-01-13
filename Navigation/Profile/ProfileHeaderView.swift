5714e9570b686773bf1f1d1d112eed7fa90b374a
Maria Mozhaeva <ridebyhorse@gmail.com>
2022-01-13 15:00:04 +0300

    Добавлен хедер для таблицы в ленте профиля

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Мария Можаева on 12.01.2022.
+//  Copyright © 2022 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderView: UITableViewHeaderFooterView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
